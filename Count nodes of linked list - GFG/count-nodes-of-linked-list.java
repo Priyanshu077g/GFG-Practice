8aa8c475d9cb9a4993e221771feb4f71ce76999b
Priyanshu077g <89195357+Priyanshu077g@users.noreply.github.com>
2023-01-26 19:22:41 +0530

    Added solution - LeetHub

--- Count nodes of linked list - GFG/count-nodes-of-linked-list.java
+++ Count nodes of linked list - GFG/count-nodes-of-linked-list.java
@@ -25,14 +25,15 @@ class Solution
     //Function to count nodes of a linked list.
     public static int getCount(Node head)
     {
-        // if(head == null) return 0;
-        int size = 0;
-        Node temp = head;
-        while(temp != null) {
-            size++;
-            temp = temp.next;
-        }
-        return size;
+        if(head == null) return 0;
+        // int size = 0;
+        // Node temp = head;
+        // while(temp != null) {
+        //     size++;
+        //     temp = temp.next;
+        // }
+        // return size;
+        return 1 + getCount(head.next);
     }
 }
     
