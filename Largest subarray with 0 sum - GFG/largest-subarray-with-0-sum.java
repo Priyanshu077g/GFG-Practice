40e46457f8b7b109e69e3fa052875d613c233c2e
Priyanshu077g <89195357+Priyanshu077g@users.noreply.github.com>
2023-01-31 19:28:20 +0530

    Added solution - LeetHub

--- Largest subarray with 0 sum - GFG/largest-subarray-with-0-sum.java
+++ Largest subarray with 0 sum - GFG/largest-subarray-with-0-sum.java
@@ -32,25 +32,64 @@ class GfG
     int maxLen(int arr[], int n)
     {
         //       sum -> index
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int maxl = 0;
-        int sum = 0;
-        int i = -1;
-        map.put(sum, i);
-        while(i < arr.length - 1) {
-            i++;
-            sum += arr[i];
-            int templ = 0;
+        // HashMap<Integer, Integer> map = new HashMap<>();
+        // int maxl = 0;
+        // int sum = 0;
+        // int i = -1;
+        // map.put(sum, i);
+        // while(i < arr.length - 1) {
+        //     i++;
+        //     sum += arr[i];
+        //     int templ = 0;
             
-            if(!map.containsKey(sum)) {
-                map.put(sum, i);
-            }else {
-                templ = i - map.get(sum);
-            }
-            if(templ > maxl) {
-                maxl = templ;
+        //     if(!map.containsKey(sum)) {
+        //         map.put(sum, i);
+        //     }else {
+        //         templ = i - map.get(sum);
+        //     }
+        //     if(templ > maxl) {
+        //         maxl = templ;
+        //     }
+        // }
+        // return maxl;
+        
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        
+        hm.put(0,-1);
+        int prefixsum = 0;
+        int max = 0;
+        for(int i = 0; i < n; i++) {
+            prefixsum += arr[i];
+            if(hm.containsKey(prefixsum)) {
+                max = Math.max(max, i - hm.get(prefixsum));
+            }else{
+                hm.put(prefixsum, i);
             }
         }
-        return maxl;
+        return max;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
 }
