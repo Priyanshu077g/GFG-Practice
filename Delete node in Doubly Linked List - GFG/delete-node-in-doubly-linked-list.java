196392004bade8cf9a5966cd9fa50a85f5b51b44
Priyanshu <89195357+Priyanshu077g@users.noreply.github.com>
2023-09-05 14:19:35 +0530

    Added solution - LeetHub

---
+++ Delete node in Doubly Linked List - GFG/delete-node-in-doubly-linked-list.java
@@ -0,0 +1,100 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.util.*;
+
+class Node
+{
+	int data;
+	Node next;
+	Node prev;
+	Node(int d)
+	{
+		data = d;
+		next = prev = null;
+	}
+}
+class Delete_Node_From_DLL
+{
+	Node head;
+	Node tail;
+	
+	void addToTheLast(Node node)
+	{
+		if(head ==  null)
+		{
+			head = node;
+			tail = node;
+		}
+		else
+		{
+			tail.next = node;
+			tail.next.prev = tail;
+			tail = tail.next;
+		}
+	}
+	
+	void printList(Node head)
+	{	Node temp = head;
+		while(temp != null)
+		{
+			System.out.print(temp.data+" ");
+			temp =  temp.next;
+		}
+		System.out.println();
+	}
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t>0)
+		{
+			int n = sc.nextInt();
+			Delete_Node_From_DLL list = new Delete_Node_From_DLL();
+			
+			int a1 = sc.nextInt();
+			Node head = new Node(a1);
+			list.addToTheLast(head);
+			
+			for(int i=1;i<n;i++)
+			{
+				int a = sc.nextInt();
+				list.addToTheLast(new Node(a));
+			}
+			a1 = sc.nextInt();
+			Solution g = new Solution();
+			//System.out.println(list.temp.data);
+			Node ptr = g.deleteNode(list.head, a1);
+			list.printList(ptr);
+			t--;
+		}
+	}
+}
+
+
+                               
+// } Driver Code Ends
+
+class Solution
+{
+ 
+    Node deleteNode(Node head,int x)
+    {
+	    int index=1;
+	    Node cur=head;
+	    while(cur != null){
+	        if(index==x){
+	            if(cur.prev != null)
+	               cur.prev.next = cur.next;
+	            else
+                    head = cur.next;
+	            if(cur.next!=null)
+	               cur.next.prev = cur.prev;
+	            return head;
+	       }
+	        index++;
+	        cur=cur.next;
+	    }
+	    return head;
+    }
+}
+
